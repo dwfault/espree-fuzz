916e0ccf94e59650c0d8212882d1019b9ea7202e
dwfault <dwfault@163.com>
2018-03-08 12:41:08 +0800

    preorder traversal

--- espree-fuzz/generator.js
+++ espree-fuzz/generator.js
@@ -4,20 +4,33 @@ const espree = require("espree");
 const fs = require("fs");
 
 
-fs.readFile('../test2.js','utf-8',function(err,jsCode){
+fs.readFile('../test3.js', 'utf-8', function (err, jsCode) {
     console.log(jsCode);
-    let ast = espree.parse(jsCode,{ecmaVersion:6});
+    let ast = espree.parse(jsCode, { ecmaVersion: 6 });
     console.log(ast);
-    console.log(ast.body[0].id);
-    
-    console.log(Object.getPrototypeOf(ast.body[0].id)===Object.getPrototypeOf(ast));//extensible node
-    console.log(Object.getPrototypeOf(ast.body[0].id));
-    console.log(Object.getOwnPropertyNames(ast.body[0].id));
 
-    let ownPropertyName = 'type';
-    console.log(ast.body[0].id[ownPropertyName]);
+    console.log('========');
+    function traverseNode(node) {
+        
+        let start = null;                                             //  important fields of the node
+        let end = null;
+        let type = null;                                              //
+
+        for (let index in node) {
+
+            if (node[index] == node)
+                return;
+            if (typeof node == "string")
+                return;
+            
+            console.log('[+] 前序遍历：' + index + ':' + node[index]);   //  dealing with fields
+            if(index == 'start'){
+                console.log('startttttt!');
+            }
+                                                                        //
+            traverseNode(node[index]);
+        }
+    }
+    traverseNode(ast);
     
-    console.log(ast.body[2].body.body[0]);
-    //let tokens = espree.tokenize(jsCode);
-    //console.log(tokens);
 });
