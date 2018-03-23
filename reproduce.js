27ca37955edc12ab2712b48533fc10402b9e11e7
dwfault <dwfault@163.com>
2018-03-23 14:35:10 +0800

    reproduce.js

--- reproduce.js
+++ reproduce.js
@@ -8,18 +8,27 @@ var crashPath = arguments[1];
 
 var files = fs.readdirSync(crashPath);
 
+var filterNos = ["asanUnsafeJSValue",];
+
 for (file of files) {
-    
+
     var reproduce = exec(fuzzeePath + ' ' + file);
-    var reproduceLog = '[-] '+file+':\n';
-    reproduce.stderr.on('data',function(data){
+    var reproduceLog = '[-] ' + file + ':\n';
+    reproduce.stderr.on('data', function (data) {
         reproduceLog += data;
     });
-    reproduce.stdout.on('data',function(data){
+    reproduce.stdout.on('data', function (data) {
         reproduceLog += data;
     });
-    reproduce.on('close',function(code,signal){
+    reproduce.on('close', function (code, signal) {
         reproduceLog += '--------------------';
-        console.log(reproduceLog);
+        var filted = false;
+        for (filterNo of fileterNos) {
+            if (reproduceLog.indexOf(filterNos) != -1) {
+                filted = true;
+            }
+        }
+        if(filted == false)
+            console.log(reproduceLog);
     });
 }
